c1646eb321dbdf563f10ae9604a6db5643b9c0c6
elmcgill <ethanlmcgill@gmail.com>
2021-10-01 14:27:27 -0500

    socialhub p1

--- SocialHubWeekOne/src/main/java/SocialHubDriver.java
+++ SocialHubWeekOne/src/main/java/SocialHubDriver.java
@@ -1,8 +1,63 @@
+import java.util.Scanner;
+
+import com.example.exceptions.InvalidCredentialsException;
+import com.example.models.User;
+import com.example.services.UserService;
 
 public class SocialHubDriver {
-
+	
+	private static UserService uServ = new UserService("users.txt");
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+		
+		boolean done = false;
+		
+		User loggedIn = null;
+		
+		while(!done) {
+			
+			if(loggedIn == null) {
+				System.out.println("Login or Signup? Press 1 to Login, Press 2 to SignUp");
+				int choice = Integer.parseInt(in.nextLine());
+				if(choice == 1) {
+					System.out.print("Please enter your username: ");
+					String username = in.nextLine();
+					System.out.print("Please enter your password: ");
+					String password = in.nextLine();
+					try {
+						loggedIn = uServ.login(username, password);
+					} catch(InvalidCredentialsException e) {
+						System.out.println("Username or password incorrect. Goodbye");
+						done = true;
+					}
+					continue;
+				}
+				else {
+					System.out.print("Please enter your first name: ");
+					String first = in.nextLine();
+					System.out.print("Please enter your last name: ");
+					String last = in.nextLine();
+					System.out.print("Please enter your username: ");
+					String username = in.nextLine();
+					System.out.print("Please enter your password: ");
+					String password = in.nextLine();
+					
+					try {
+						loggedIn = uServ.signUp(first, last, username, password);
+						System.out.println("You may now login with the username: " + loggedIn.getUsername());
+						loggedIn = null;
+						continue;
+					} catch (Exception e) {
+						System.out.println("Sorry we could not process your request");
+						System.out.println("Please try again later");
+						continue;
+					}
+				}
+			}
+			
+		}
 
 	}
 
---
+++ SocialHubWeekOne/src/main/java/com/example/services/UserService.java
@@ -0,0 +1,85 @@
+package com.example.services;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import com.example.dao.FileIO;
+import com.example.exceptions.InvalidCredentialsException;
+import com.example.exceptions.UsernameAlreadyExistsException;
+import com.example.logging.Logging;
+import com.example.models.User;
+
+public class UserService {
+	
+	private String file;
+	private FileIO<User> io;
+	
+	public UserService(String file) {
+		this.file = file;
+		this.io = new FileIO<User>(file);
+	}
+	
+	public User signUp(String firstName, String lastName, String username, String password) {
+		
+		ArrayList<User> users;
+		
+		try {
+			users = io.readObjects();
+		} catch(FileNotFoundException e) {
+			System.out.println("Creating a blank arraylist");
+			users = new ArrayList<User>();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		//Check to see if the users username is already stored
+		for(int i=0; i<users.size(); i++) {
+			if(users.get(i).getUsername().equals(username)) {
+				//Throw an exception that username already exists
+				Logging.logger.warn("Username created that already exists in the database");
+				throw new UsernameAlreadyExistsException();
+			}
+		}
+		
+		User u = new User(firstName, lastName, username, password);
+		
+		users.add(u);
+		io.writeObjects(users);
+		
+		return u;
+	}
+	
+	public User login(String username, String password) throws InvalidCredentialsException {
+		
+		ArrayList<User> users;
+		
+		try {
+			users = io.readObjects();
+		} catch(FileNotFoundException e) {
+			users = new ArrayList<User>();
+		} catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		for(int i=0; i<users.size(); i++) {
+			//If the username exists in the file, then we want to check the password
+			if(users.get(i).getUsername().equals(username)) {
+				if(users.get(i).getPassword().equals(password)) {
+					Logging.logger.info("User: " + username + " was logged in");
+					System.out.println("User was signed in");
+					return users.get(i);
+				} else {
+					Logging.logger.warn("User tried logging in with invalid credentials");
+					//Throw an invalidcredentialsexception
+					throw new InvalidCredentialsException();
+				}
+			}
+		}
+		Logging.logger.warn("User tried signing in with invalid username");
+		throw new InvalidCredentialsException();
+	}
+	
+}
