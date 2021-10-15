2b94a3b15faadc33ae3d158a07b858df589b60a4
elmcgill <ethanlmcgill@gmail.com>
2021-10-15 11:35:04 -0500

    week 3 notes finished

---
+++ WeekThree/WebDay5.md
@@ -0,0 +1,45 @@
+# fetch
+
+fetch is a more modern and versatile way of making AJAX requests
+The fetch API is provided by the window object, and provides the `.fetch()` method
+
+- `.fetch()` is used to send requests and returns a promise that is retrieved from the response
+- Successful requests get resolved, the returned promise will have the response body
+- Request that return an error HTTP-Status will be rejected
+
+There are various methods you can use to access the response body of a success request:
+- `response.text()` read the response and return as text
+- `response.json()` parse the response as JSON
+- `response.formData()` return the response as FormData object
+- `response.blob()` return the response as Blov (binary data with type)
+- `response.arrayBuffer()` return the response as an ArrayBuffer (low level representation of binary data)
+
+# async/await
+introduced in ES8 to simplify asynchronous functionality
+
+The async keyword denotes that a function will operate asynchronously via the event loop
+- Any function with the async keyword will implicitly return a promise object
+- Yuo can use any of the consumer function on the return of an async function, however there are nicer ways to handle these promises using await
+
+The await keyword only works inside of async functions
+- await tells Javascript to wait for a promise to resolved before moving on
+- You can use await when calling any function that returns a promise, but it must be inside of an async function
+- It allows you to gather data, or wait for something while also not blocking the main thread
+
+You no longer need to chain consumer functions together, making your code easier to read
+
+# Timing Events
+are functions that are attached to the global window object, and allow programmers to automate or run tasks after waiting a specific amount of time
+
+These get handled by the event loop and web threads
+
+## setTimeout()
+
+`window.setTimeout(callbackFunction, milliseconds)` will execute the callback function after waiting for a specific amount of milliseconds
+
+Use the `window.clearTimeout(timeoutVariable)` to stop a timeout before the execution of the callback function
+
+## setInterval()
+`window.setInterval(callbackFunction, milliseconds)` continually executes the callback function ever so many milliseconds
+
+Use `window.clearInterval(intervalVariable)` to stop the interval, similar clearTimeout
--- WeekThreeDemos/AJAX/index.js
+++ WeekThreeDemos/AJAX/index.js
@@ -15,6 +15,7 @@ function populatePage(posts){
 
 let apiURL = 'https://jsonplaceholder.typicode.com/posts';
 
+/*
 //When our document loads we will make the request
 //There are 4 steps to making an ajax request
 (()=> {
@@ -36,4 +37,103 @@ let apiURL = 'https://jsonplaceholder.typicode.com/posts';
 
     //step four: send the request
     xhttp.send();
+})();
+*/
+
+/*
+//Refactor the above with Fetch and promises
+(() => {
+    fetch(apiURL)
+        .then((res) => res.json())
+        .then((data) => populatePage(data));
+})();
+
+
+//Lets take a look at the response headers
+(function responseHeaders(){
+    fetch(apiURL).then((res) => console.log(res.headers));
+})();
+
+//We can set request headers including an object in the fetch call
+(function requestHeaders(){
+    fetch(apiURL,
+        {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((res) => res.json())
+        .then((json) => console.log(json));
+})();
+
+(function post(){
+    let title = 'This is our title value';
+    let body = 'This is our body value';
+    let obj = {
+        title,
+        body,
+        id: 1
+    };
+
+    fetch(apiURL,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+    })
+    .then((res) => res.json())
+    .then((json) => console.log(json));
+})();
+
+(function failedRequest(){
+    fetch(apiURL, {
+        method: 'GET',
+        body: {}
+    })
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.log('Do some logic if error occurs'));
+})();
+*/
+
+//Refactor once more with async/await
+(async () => {
+    let req = await fetch(apiURL);
+    let res = await req.json();
+    populatePage(res);
+})();
+
+( async function post(){
+    let title = 'This is our title value';
+    let body = 'This is our body value';
+    let obj = {
+        title,
+        body,
+        id: 1
+    }
+
+    let req = await fetch(apiURL,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+    });
+
+    let res = await req.json();
+    console.log(res);
+})();
+
+(async function failedRequest(){
+    try{
+        let req = await fetch(apiURL, {
+            method: 'GET',
+            body: {}
+        });
+        let res = await req.json();
+        console.log(res);
+    } catch(e){
+        console.log('An error occured');
+    }
 })();
