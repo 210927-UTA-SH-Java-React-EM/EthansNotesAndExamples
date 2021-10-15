c6cad002930748c1ed311f05aa325e085bfaf071
samesma <saeid.esma@gmail.com>
2021-10-15 12:15:57 -0700

    Hands on 3

--- ho3/ho3.js
+++ ho3/ho3.js
@@ -1,15 +1,20 @@
 let container = document.getElementById('placeholder');
 
-function populatePage(posts){
-    for(postObj of posts){
+function populatePage(posts,res2){
+   
+    console.log(posts.forms);
         let post = document.createElement('div');
-        post.innerHTML = `<h2>${postObj.title}</h2><p>${postObj.body}</p>`;
+        post.innerHTML = `<h2 class="flex-child magenta">${posts.forms[0].name}</h2>`;
+        post.innerHTML +=`<img class="flex-child green" src=${res2.sprites.front_default} />`;
+        post.innerHTML += `<br/><img class="flex-child green" src=${res2.sprites.back_default} />`;
         container.append(post);
-    }
+    
 }
 
 
 let apiURL = 'https://pokeapi.co/api/v2/pokemon/';
+let apiURL2 = 'https://pokeapi.co/api/v2/pokemon-form/';
+
 
 
 
@@ -22,12 +27,15 @@ function submitForm(){
     
     console.log(apiURL+pokid.value);
 
-    async () => {
+    (async () => {
         let req = await fetch(apiURL+pokid.value);
         let res = await req.json();
-        populatePage(res);
-        //console.log(res.);
-    };
+
+        let req2 = await fetch(apiURL2+pokid.value);
+        let res2 = await req2.json();
+        populatePage(res,res2);
+        //console.log(res);
+    })();
 
     /*let placeholder = document.getElementById('placeholder');   
     placeholder.innerHTML += "<br/> Hello " + document.getElementById('username').value +", Hobbies: "
