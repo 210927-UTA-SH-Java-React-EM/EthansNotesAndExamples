a2c689f603304a0682f5a99e549f9dd208c31e1c
elmcgill <ethanlmcgill@gmail.com>
2021-10-19 16:24:31 -0500

    social hub week 4

--- SocialHubWeekFour/src/main/java/com/example/servlets/Dispatcher.java
+++ SocialHubWeekFour/src/main/java/com/example/servlets/Dispatcher.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.example.controllers.LoginController;
+import com.example.controllers.PostController;
+import com.example.controllers.SessionController;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 public class Dispatcher {
@@ -16,6 +18,11 @@ public class Dispatcher {
 			case "/SocialHubWeekFour/api/login":
 				LoginController.login(req, res);
 				break;
+			case "/SocialHubWeekFour/api/posts":
+				PostController.handlePosts(req, res);
+				break;
+			case "/SocialHubWeekFour/api/session":
+				SessionController.getSession(req, res);
 		}
 	}
 	
