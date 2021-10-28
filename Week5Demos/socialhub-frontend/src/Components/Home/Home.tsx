18f53c80c5d1a35e516354341d792596c07d156d
elmcgill <ethanlmcgill@gmail.com>
2021-10-28 13:09:12 -0500

    redux example finished

--- Week5Demos/socialhub-frontend/src/Components/Home/Home.tsx
+++ Week5Demos/socialhub-frontend/src/Components/Home/Home.tsx
@@ -1,6 +1,9 @@
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {getPosts} from '../../Actions/PostActions';
+import {useHistory} from 'react-router-dom';
+
+import {Post} from '../Post/Post';
 
 import './Home.css';
 
@@ -9,7 +12,12 @@ export const Home: React.FC<any> = () => {
     const appState = useSelector<any, any>((state) => state);
     const dispatch = useDispatch();
 
+    const history = useHistory();
+
     useEffect(() => {
+        if(appState.user.id <= 0){
+            history.push('/');
+        }
         console.log(appState);
         loadPosts();
     }, [appState.posts.length]);
@@ -23,10 +31,10 @@ export const Home: React.FC<any> = () => {
     return (
         <div className="home-page">
             <div className="home-container">
-                <h1>Welcome to social hub</h1>
+                <h1>Welcome {appState.user.username}</h1>
                 {appState.posts.map((post:any) => {
                     return(
-                        <h1>Placeholder</h1>
+                        <Post {...post} key={post.postId} />
                     );
                 })}
             </div>
